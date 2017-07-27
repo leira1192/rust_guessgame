1c396e1b554a94db2f8e7821e8e8cfe37fe86936
leira1192 <leira1192@gmail.com>
2017-07-27 14:27:43 -0600

    add point ingeniero y mejor print de intentos finales

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     	let aleatorio = rand::thread_rng().gen_range(1,1001); //se toma un numero de un hilo de un rango
 		//println!("El numero a adivinar es: {}", aleatorio); //En caso de querer saber que numero generado
 		println!("====================== Adivina el Numero ======================");
-		println!("Instrucciones: Adivina el numero en un rango del 1 al 1000, suerte!");
+		println!("Instrucciones: Se ha ganado un numero aleatorio en un rango del 1 al 1000, intenta adivinarlo, suerte!");
 		let mut contador = 0; //contador que registra el numero de intentos realizados
 	    let mut continuar = true; //Boolean para continuar el juego
     	while continuar {
@@ -18,7 +18,7 @@ fn main() {
 	    	let mut line = String::new(); //almacenador de lectura
 	    	io::stdin().read_line(&mut line)
 	    	.ok()
-	    	.expect("No se pudo leer la linea"); //Read
+	    	.expect("No se pudo leer la linea"); //lee la linea y lanza excepcion si ocurre un error
 	    	let line: u32 = line.trim().parse().ok().expect("Favor ingresar un numero");
 	    	match line.cmp(&aleatorio) { //comparador de el numero secreto con el imput
 				Ordering::Less => println!("Demasiado Bajo, intenta de nuevo"),
@@ -30,11 +30,11 @@ fn main() {
 			}//fin del if
 	    }//fin while continuar
 	    if contador < 10 { //contador para dar la clasificacion y el comentario acerca del numero de intentos
-			println!("Ya sabia ud el secreto, o tuvo suerte!");
+			println!("lo hizo en: {} intento(s),ya sabia ud el secreto, o tuvo suerte!.", contador);
 		}else if contador == 10 {
-			println!("Aja!, sabia ud el secreto");
+			println!("Aja!, sabia ud el secreto, lo logro en {} intento(s).", contador);
 		}else {
-			println!("Deberia haberlo hecho mejor!");
+			println!("Deberia haberlo hecho mejor!, lo adivino en {} intento(s)", contador);
 		}// fin if else
 		let mut pregunta = true;
 		while pregunta {
@@ -51,7 +51,8 @@ fn main() {
 					pregunta = false; // para salir de pregunta cuando elija
 			}else {
 				println!("Elija una opcion"); // tiene que elegir una opcion a fuerza
-			}	
+			}// fin del if else
 		}// fin del while de pregunta
     }//fin while reiniciar
+    println!("Un puntito ingeniero!!!"); //No sea asi ingeniero, suelte ese puntito extra
 }//fin main
