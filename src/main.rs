b93704a5c20d1d24451a4b5e5a027d603fa377bb
leira1192 <leira1192@gmail.com>
2017-07-27 01:09:14 -0600

    finish rust proyect 2.0 correccion de errores

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@ extern crate rand;
 use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
-//use std::string;
-
 
 fn main() {
-	let mut reiniciar = true;
-	while reiniciar {
-    	let aleatorio = rand::thread_rng().gen_range(1,1001);
-		println!("El numero a adivinar es: {}", aleatorio);
-		println!("Adivina el Numero");
+	let mut reiniciar = true; //variable
+	while reiniciar { //Ciclo para reiniciar el juego
+    	let aleatorio = rand::thread_rng().gen_range(1,1001); //se toma un numero de un hilo de un rango
+		//println!("El numero a adivinar es: {}", aleatorio); //En caso de querer saber que numero generado
+		println!("====================== Adivina el Numero ======================");
 		println!("Instrucciones: Adivina el numero en un rango del 1 al 1000, suerte!");
 		let mut contador = 0; //contador que registra el numero de intentos realizados
 	    let mut continuar = true; //Boolean para continuar el juego
@@ -27,11 +25,11 @@ fn main() {
 				Ordering::Greater => println!("Demasiado Alto, intenta de nuevo"),
 				Ordering::Equal => println!("Adivinaste!!"),
 			}//fin match
-			if aleatorio == line {
-				continuar = false;
+			if aleatorio == line { //comprobar que acerto en el resultado
+				continuar = false; //salir de los intentos
 			}//fin del if
 	    }//fin while continuar
-	    if contador < 10 {
+	    if contador < 10 { //contador para dar la clasificacion y el comentario acerca del numero de intentos
 			println!("Ya sabia ud el secreto, o tuvo suerte!");
 		}else if contador == 10 {
 			println!("Aja!, sabia ud el secreto");
@@ -41,19 +39,19 @@ fn main() {
 		let mut pregunta = true;
 		while pregunta {
 			println!("Desea volver a jugar? (s/n)");
-			let mut play_again = String::new();
+			let mut play_again = String::new(); //se declara una variable inmutable de tipo string
 			io::stdin().read_line(&mut play_again)
 			.ok()
-			.expect("No se pudo leer la linea"); // Read continuous
-			if play_again.starts_with("s").to_string() == "true" || play_again.starts_with("S").to_string() == "true"{
-				reiniciar = true;
-				pregunta = false;
-			}else if play_again.starts_with("n").to_string() == "true" || play_again.starts_with("N").to_string() == "true" {
-					reiniciar = false;
-					pregunta = false;
+			.expect("No se pudo leer la linea"); // lee respuesta a reiniciar
+			if play_again.starts_with("s").to_string() == "true" || play_again.starts_with("S").to_string() == "true"{ //comprobar mediante string si elige si
+				reiniciar = true; //salir del programa
+				pregunta = false; //salir de pregunta
+			}else if play_again.starts_with("n").to_string() == "true" || play_again.starts_with("N").to_string() == "true" { //comprobar mediante string si elige no
+					reiniciar = false; // salir del programa
+					pregunta = false; // para salir de pregunta cuando elija
 			}else {
-				println!("Elija una opcion");
+				println!("Elija una opcion"); // tiene que elegir una opcion a fuerza
 			}	
-		}
+		}// fin del while de pregunta
     }//fin while reiniciar
 }//fin main
